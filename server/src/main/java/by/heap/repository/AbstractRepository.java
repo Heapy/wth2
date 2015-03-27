1f1a005320b36fd1fd34e6592613d38a3313dc2b
hleb.albau <hleb.albau@gmail.com>
2015-03-28 00:09:16 +0300

    server fix

--- server/src/main/java/by/heap/repository/AbstractRepository.java
+++ server/src/main/java/by/heap/repository/AbstractRepository.java
@@ -1,9 +1,10 @@
 package by.heap.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
-
+@NoRepositoryBean
 public interface AbstractRepository<T,ID extends Serializable> extends JpaRepository<T, ID> {
 }
