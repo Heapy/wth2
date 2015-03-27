fcc139d274664acbb7b0eb812e697ae270fd5882
hleb.albau <hleb.albau@gmail.com>
2015-03-28 01:30:42 +0300

    adventure service

---
+++ server/src/main/java/by/heap/service/AdventureService.java
@@ -0,0 +1,9 @@
+package by.heap.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/adventure")
+public class AdventureService {
+}
