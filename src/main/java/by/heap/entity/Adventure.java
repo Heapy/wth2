3ed478830cf188a476f14d654b5891eee7614ed2
Ruslan Ibragimov <theliveperson@gmail.com>
2015-03-28 15:03:48 +0300

    Refactor.

--- src/main/java/by/heap/entity/Adventure.java
+++ src/main/java/by/heap/entity/Adventure.java
@@ -4,10 +4,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * TODO: CommentMe!
- *  @Deprecated see SecurityConfig.passwordEncoder
+ *
  * @author Ibragimov Ruslan
  */
-@Deprecated
 public class Adventure {
 
     private User firstUser;
