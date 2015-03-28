5fea9a2951ed777344b2243805fe600744bf8d7c
hleb.albau <hleb.albau@gmail.com>
2015-03-28 19:44:35 +0300

    user security

--- src/main/java/by/heap/SecurityConfig.java
+++ src/main/java/by/heap/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/auth/login")
                 .antMatchers(HttpMethod.GET, "/users/top")
                 .antMatchers(HttpMethod.GET, "/users/location")
+                .antMatchers(HttpMethod.GET, "/user/**")
                 .antMatchers(HttpMethod.GET, "/")
                 .antMatchers(HttpMethod.GET, "/*.html")
                 .antMatchers(HttpMethod.GET, "/assets/**");
