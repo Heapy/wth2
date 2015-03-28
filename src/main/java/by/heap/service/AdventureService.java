a0b18e3869f9eba88afd75651354eb919125a8fe
Ruslan Ibragimov <theliveperson@gmail.com>
2015-03-29 00:30:12 +0300

    Fixes in adventure.

--- src/main/java/by/heap/service/AdventureService.java
+++ src/main/java/by/heap/service/AdventureService.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
