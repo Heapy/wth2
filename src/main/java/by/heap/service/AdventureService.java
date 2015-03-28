ece5c278c36b7238f2103181242baedec4ff6e21
Uladzislau_Hayeuski <Uladzislau_Hayeuski@epam.com>
2015-03-28 16:25:11 +0300

    find adventure logic implemented

--- src/main/java/by/heap/service/AdventureService.java
+++ src/main/java/by/heap/service/AdventureService.java
@@ -3,6 +3,8 @@ package by.heap.service;
 import by.heap.entity.Adventure;
 import by.heap.entity.User;
 import by.heap.security.HeapApplicationContext;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping(value = "/adventure", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -24,6 +27,8 @@ public class AdventureService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventureService.class);
 
     private static final Set<UserHolder> USER_HOLDERS = Collections.newSetFromMap(new ConcurrentHashMap<>());
+
+    private IntStream randomTokens = new Random().ints().filter(i -> i > 100000 && i < 999999);
 
     @Autowired
     private HeapApplicationContext applicationContext;
@@ -46,6 +51,45 @@ public class AdventureService {
             }
         }
         return currentAdventure;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Adventure findMeAdventureOnMyAss() {
+        final User currentUser = applicationContext.getCurrentUser();
+        Adventure foundAdventure = null;
+        for (UserHolder userHolder : USER_HOLDERS) {
+            User userToCompare = userHolder.getAdventure().getFirstUser();
+            if (hasSameInterests(currentUser, userToCompare)) {
+                if (userHolder.getAdventure().getStatus().compareAndSet(false, true)) {
+                    foundAdventure = userHolder.getAdventure();
+                    foundAdventure.setSecondUser(currentUser);
+                    USER_HOLDERS.remove(userHolder);
+                    //TODO в список играющих
+                    break;
+                }
+            }
+        }
+        if (foundAdventure == null) {
+            foundAdventure = createNewAdventureAndWait(currentUser);
+        }
+        return foundAdventure;
+    }
+
+    private boolean hasSameInterests(User user1, User user2) {
+        return !Sets.intersection(new HashSet<>(user1.getInterests()), new HashSet<>(user2.getInterests())).isEmpty();
+    }
+
+    private Adventure createNewAdventureAndWait(User user) {
+        Adventure newAdventure = new Adventure()
+            .setFirstUser(user)
+            .setStatus(new AtomicBoolean(false))
+            .setToken(String.valueOf(randomTokens.findAny().getAsInt()));
+        USER_HOLDERS.add(
+            new UserHolder()
+                .setInstant(Instant.now())
+                .setAdventure(newAdventure)
+        );
+        return newAdventure;
     }
 
 }
