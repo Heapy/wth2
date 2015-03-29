07b6ca5e9c190345ad9b7738d76c20a034dbb387
Ruslan Ibragimov <theliveperson@gmail.com>
2015-03-29 14:09:09 +0300

    Fix Adventures controller.

--- src/main/java/by/heap/service/AdventureService.java
+++ src/main/java/by/heap/service/AdventureService.java
@@ -59,8 +59,12 @@ public class AdventureService {
     @Scheduled(fixedRate = 30000)
     public void checkForOutdatedUsersInGame() {
         for (Adventure adventure : ADVENTURES) {
-            userRepository.save(adventure.getFirstUser());
-            userRepository.save(adventure.getSecondUser());
+            if (Objects.nonNull(adventure.getFirstUser())) {
+                userRepository.save(adventure.getFirstUser());
+            }
+            if (Objects.nonNull(adventure.getSecondUser())) {
+                userRepository.save(adventure.getSecondUser());
+            }
             // If Adventure has expired user and status of adventure is true (started)
             if (GameStatus.PLAYING.equals(adventure.getGameStatus()) && isFirstUserExpired(adventure, 60)) {
                 LOGGER.info("Adventure with id = '{}' have expired first user with username = '{}'.", adventure.getId(), adventure.getFirstUser().getUsername());
